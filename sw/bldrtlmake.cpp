d7e8c2cabd6ccb77dc927dd805bf3fe43239371c
ZipCPU <dgisselq@ieee.org>
2017-08-12 19:29:12 -0400

    Fixed the missing make group name bug
    If the make group name is missing, a random group name will be assigned
    now via mkstemp.

--- sw/bldrtlmake.cpp
+++ sw/bldrtlmake.cpp
@@ -99,6 +99,18 @@ void	build_rtl_make_inc(MAPDHASH &master, FILE *fp, STRING &fname) {
 		} if (filstr[filstr.size()-1] == ' ')
 			filstr[filstr.size()-1] = '\0';
 		if (mksubd) {
+			if (!mkgroup) {
+				char	temp[] = "MKGRPXXXXXX";
+				mkstemp(temp);
+				mkgroup = new STRING(temp);
+				setstring(kvpair->second, KYRTL_MAKE_GROUP, mkgroup);
+
+				STRINGP pfx = getstring(kvpair->second.u.m_m, KYPREFIX);
+				if (NULL == pfx)
+					pfx = new STRING("(Unnamed)");
+
+				fprintf(stderr, "WARNING: Creating a temporary group name for %s\n", pfx->c_str());
+			}
 			fprintf(fp, "%sD := %s\n", mkgroup->c_str(),
 				mksubd->c_str());
 			fprintf(fp, "%s  := $(addprefix $(%sD)/,%s)\n",
